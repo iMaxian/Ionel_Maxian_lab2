f5c9cde4a63d12abf02ce52b7dca554d92eb6ded
Ionel <Ionel@DESKTOP-E6VJGNI>
2023-10-23 17:13:18 +0300

    Add project files.

---
+++ Data/Ionel_Maxian_lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ionel_Maxian_lab2.Models;
+
+namespace Ionel_Maxian_lab2.Data
+{
+    public class Ionel_Maxian_lab2Context : DbContext
+    {
+        public Ionel_Maxian_lab2Context (DbContextOptions<Ionel_Maxian_lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ionel_Maxian_lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Ionel_Maxian_lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
