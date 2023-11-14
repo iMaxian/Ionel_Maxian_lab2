ac76ec8525e24f1279e684c5fce50a35f1278378
IONEL MAXIAN <ionel.maxian@stud.ubbcluj.ro>
2023-11-14 18:53:29 +0200

    lab5_finish

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Ionel_Maxian_lab2.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Ionel_Maxian_lab2Context>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Ionel_Maxian_lab2Context") ?? throw new InvalidOperationException("Connection string 'Ionel_Maxian_lab2Context' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Ionel_Maxian_lab2Context") 
+       ?? throw new InvalidOperationException("Connection string 'Ionel_Maxian_lab2Context' not found.")));
+
+builder.Services.AddDbContext<LibraryIdentityContext>(options => 
+  options.UseSqlServer(builder.Configuration.GetConnectionString("Ionel_Maxian_lab2Context") 
+  ?? throw new InvalidOperationException("Connection" +
+  " string 'Nume_Pren_Lab2Context' not found."))); 
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => 
+  options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<LibraryIdentityContext>();
+
+
 
 var app = builder.Build();
 
@@ -22,6 +35,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
